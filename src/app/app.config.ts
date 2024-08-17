e8590887ce4976b7e72f678c7e288a26addd52e3
Omar Adel <69749119+Omar798-c@users.noreply.github.com>
2024-08-17 14:57:03 +0300

    preparing the environment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,27 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideHttpClient } from '@angular/common/http';
+import { provideStoreDevtools } from "@ngrx/store-devtools";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection(
+      {
+        eventCoalescing: true
+      }
+    ),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideStore(),
+    provideEffects(),
+    provideHttpClient(),
+    provideStoreDevtools(
+      {
+        maxAge: 25
+      }
+    ),
+  ]
 };
